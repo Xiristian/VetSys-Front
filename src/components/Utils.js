b5a274fd5df4fb17dbe562b7905a8c15e37162cd
Xiristian <christianhederson22@gmail.com>
2023-12-05 23:23:55 -0300

    Ajustes Edição de atendimento

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,3 +1,4 @@
 export function convertBackendDateToFrontend(backendDate) {
+    if (!backendDate) return null;
     return `${backendDate[0]}-${backendDate[1].toString().padStart(2, '0')}-${backendDate[2].toString().padStart(2, '0')}`;
 }
